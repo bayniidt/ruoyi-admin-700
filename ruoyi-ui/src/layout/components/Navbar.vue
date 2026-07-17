221bb2c91058b4129c58553034f21d798e169d57
cc <331201489@qq.com>
2026-07-17 10:39:37 +0800

    chore

--- ruoyi-ui/src/layout/components/Navbar.vue
+++ ruoyi-ui/src/layout/components/Navbar.vue
@@ -118,7 +118,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index'
+          this.$router.replace('/login')
         })
       }).catch(() => {})
     }
